e28b1285a669067acb24aa48cab306e4e7950787
Amogh Kapoor <chatur.chaturx@gmail.com>
2021-09-25 10:06:51 +0530

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Home, Login, Register } from "./pages"
-import { Navbar } from "./components"
+import Router from "./Router"
 import { AuthProvider } from './context/auth'
 import "./styles/defaults/app.scss"
 
 const App = () => {
   return (
     <AuthProvider>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch >
-      </Router>
+      <Router />
     </AuthProvider>
   )
 }
